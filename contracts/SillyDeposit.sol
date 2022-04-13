77424827aecd06abf569955bb39f7cf12231e4fc
Andrey Ezhov <rybakisa@gmail.com>
2022-04-13 07:56:53 +0300

    Add simple deposit contract
    Add silly lending contract.
    Simple locking and interest rate calculation logic implemented.

---
+++ contracts/SillyDeposit.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+contract SillyDepositContract {
+
+    uint public depositVolume;
+    uint public depositDate;
+    uint public interestRate;
+    uint public lockPeriod;
+    address public contractOwner;
+    address public depositOwner;
+
+    constructor(uint _lockPeriod, uint _interestRate) {
+        contractOwner = msg.sender;
+        lockPeriod = _lockPeriod;
+        interestRate = _interestRate;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function receiveDeposit() public payable {
+        if (depositOwner == address(0)) {
+            depositOwner = payable(msg.sender);
+            depositVolume += msg.value;
+            depositDate = block.timestamp;
+        }
+        else if (depositOwner == payable(msg.sender)) {
+            revert('You can't refund this deposit!');
+        }
+        else {
+            revert('This deposit already owned!');
+        }
+    }
+
+    function receiveFunds() public payable {
+    }
+
+    function compoundInterest() private view returns(uint) {
+        return depositVolume + depositVolume * interestRate / 100;
+    }
+
+    function withdrawDeposit(uint _withdrawAmountInPercents) public {
+        require(block.timestamp-depositDate < lockPeriod, 'Your funds still locked');
+        require(depositOwner == msg.sender, 'You are not owner of this deposit!');
+
+        uint fullAmount = compoundInterest();
+        uint amountToSend = fullAmount * _withdrawAmountInPercents / 100;
+        depositVolume = fullAmount - amountToSend;
+
+        address payable to = payable(msg.sender);
+        to.transfer(amountToSend);
+    }
+    
+}
